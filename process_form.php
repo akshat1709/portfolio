e97a697db2ea38254f16f23735c1b1093744b704
Akshat Gupta <guptaakshat1709@gmail.com>
2024-06-14 21:21:50 +0530

    contact form update

--- process_form.php
+++ process_form.php
@@ -10,21 +10,36 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: ". $conn->connect_error);
 }
 
 // Process form data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["clientemail"];
     $phone = $_POST["clientphone"];
-    $collaborate = isset($_POST["isclient"]) ? 1 : 0;
+    $collaborate = isset($_POST["isclient"])? 1 : 0;
 
     // Insert data into the database
     $sql = "INSERT INTO contacts (email, phone, collaborate) VALUES ('$email', '$phone', $collaborate)";
     if ($conn->query($sql) === TRUE) {
-        echo "Form submitted successfully";
+        // Send email notification
+        $to = "guptaakshaat1709@gmail.com"; 
+        $subject = "New Contact Form Submission";
+        $message = "Someone tried contacting you:\n\n";
+        $message.= "Email: $email\n";
+        $message.= "Phone: $phone\n";
+        $message.= "Collaborate: ". ($collaborate? "Yes" : "No"). "\n";
+
+        $headers = "From: $email\r\n";
+        $headers.= "Reply-To: $email\r\n";
+        $headers.= "MIME-Version: 1.0\r\n";
+        $headers.= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        mail($to, $subject, $message, $headers);
+
+        echo "Form submitted successfully. You will receive an email notification.";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: ". $sql. "<br>". $conn->error;
     }
 }
 
